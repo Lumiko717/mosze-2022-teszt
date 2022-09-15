2abda4e016147ab20d823ce372fe625263a6ac6a
Lumiko717 <lumiko717@gmail.com>
2022-09-15 16:57:53 +0200

    main.cpp hibak commentelve

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; 	// valtozo neve NELEMENTS elírva N_ELEMENTS helyett
+    std::cout << '1-100 ertekek duplazasa' 	// cout parancs szoveg '_' kozbefogva "_" helyett valamint nincs ; a sor vegen
+    for (int i = 0;)	// for ciklusbol hianyzik a valtozo es a leptetesi ertek
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"	// hianyzik a ; a sor vegen
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)		// sima , a ciklusban ; elvalaszto helyett
     {
-        atlag += b[i]
-    }
+        atlag += b[i]		// hianyzik a ; a sor vegen
+    }	
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
